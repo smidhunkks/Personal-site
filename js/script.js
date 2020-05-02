c390aeaae55f8b207640a2570bb98169a324df7e
midhun <jmsmidhunkrishna@gmail.com>
2020-05-02 17:08:31 +0530

    scroll corrected

--- js/script.js
+++ js/script.js
@@ -10,11 +10,36 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+var x=document.getElementsByClassName("clr");
+var l=x.length
+var y=document.getElementsByClassName("btclr");
+console.log(x);
+console.log(l);
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    
+    header.classList.add("stick");
+    for(var i=0;i<l;i++){
+      if(i<3)
+      y[i].style.backgroundColor="black";
+      x[i].style.color="black";
+      
+    }
+   
+    
   } else {
-    header.classList.remove("sticky");
+    header.classList.remove("stick");
+    for(var i=0;i<l;i++){
+      if(i<3)
+      y[i].style.backgroundColor="white";
+      x[i].style.color="white"
+    }
+    
+    
+      
+    
+    
+    
   }
 }
 
