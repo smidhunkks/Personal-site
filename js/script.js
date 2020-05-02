f29194cfbc92fd7cea3372d0164d3d5ff0738529
midhun <jmsmidhunkrishna@gmail.com>
2020-05-02 21:49:08 +0530

    coming soon

--- js/script.js
+++ js/script.js
@@ -11,38 +11,38 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-var x=document.getElementsByClassName("clr");
-var l=x.length
-var y=document.getElementsByClassName("btclr");
+var x = document.getElementsByClassName("clr");
+var l = x.length
+var y = document.getElementsByClassName("btclr");
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    
+
     header.classList.add("stick");
-    for(var i=0;i<l;i++){
-      if(i<3)
-      y[i].style.backgroundColor="black";
-      x[i].style.color="black";
+    for (var i = 0; i < l; i++) {
+      if (i < 3)
+        y[i].style.backgroundColor = "black";
+      x[i].style.color = "black";
       x[i].classList.add("blk");
-      
-      
+
+
     }
-   
-    
+
+
   } else {
     header.classList.remove("stick");
-    for(var i=0;i<l;i++){
-      if(i<3)
-      y[i].style.backgroundColor="white";
-      x[i].style.color="white"
+    for (var i = 0; i < l; i++) {
+      if (i < 3)
+        y[i].style.backgroundColor = "white";
+      x[i].style.color = "white"
       x[i].classList.remove("blk");
     }
-    
-    
-      
-    
-    
-    
+
+
+
+
+
+
   }
 }
 
@@ -50,3 +50,10 @@ function autoResizeDiv() {
   document.getElementById('main').style.height = window.innerHeight + 'px';
 }
 
+/*Menu icon animtion */
+function myFunction(x) {
+  
+  x.classList.toggle("change");
+  
+}
+/*Menu icon animation */
